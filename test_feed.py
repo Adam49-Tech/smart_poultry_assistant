a58fb6077d0ccb3d617319c93a0cb174fb104e42
Adam49-Tech <nnmaaward@gmail.com>
2025-07-16 17:48:05 +0100

    Clean version for Streamlit deployment

--- test_feed.py
+++
@@ -1,6 +0,0 @@
-from utils.feed_calculator import get_feed_recommendation, load_feed_data
-age = 20
-feed = get_feed_recommendation(age)
-print(f"Feed for age {age} days: {feed}")
-df = load_feed_data("my data/feed_nutrients.csv")
-print(df[df["Feed Type"] == feed])
